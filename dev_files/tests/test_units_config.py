fdcf81aa765904b2b253d99950b3cbe66492d4c3
SOUFFLET CLEMENT <clement.soufflet@meteo.fr>
2026-04-24 15:40:57 +0200

    Remove dev_files from main

--- dev_files/tests/test_units_config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from frameit.core.settings_class import SimulationConfig
-
-
-def test_build_pattern_contains_wildcards():
-    conf = SimulationConfig.from_yaml(Path("configs_ktests/arome_oper_batsirai.yml"))
-    pattern = conf.build_pattern()
-    assert "???" in pattern or "*" in pattern
